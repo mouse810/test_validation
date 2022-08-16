21e50afa21178170bc22c3ef09b72655f4df66ab
Chetan Chouhan <mousegamingv2.0@gmail.com>
2022-08-16 18:34:18 +0530

    HEll Yes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 // import FirstForm from './components/FirstForm';
-// import YupForm from './components/YupForm'
-import RegistrationView from './components/RegistrationView'
+// import RegistrationView from './components/RegistrationView'
+import YupForm from './components/YupForm'
 
 function App() {
   return (
     <div className='App'>
-      {/* <h3> hello</h3> */}
+      <h3> hello</h3>
       {/* <FirstForm /> */}
-      {/* <YupForm /> */}
-      <RegistrationView/>
+      {/* <RegistrationView/> */}
+      <YupForm />
     </div>
   );
 }
-
 export default App;
